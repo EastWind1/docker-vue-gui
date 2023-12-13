4448a9457006d4779b60c5ecd0b281b7fc59f3e9
EastWind1 <657857337@qq.com>
2023-12-13 11:46:40 +0800

    upgrade electron version

--- src/store.ts
+++ src/store.ts
@@ -3,9 +3,18 @@ import { ref } from 'vue';
 import {Subject} from 'rxjs';
 export const useStateStore = defineStore('state', () => {
     const isDockerRunning = ref(false);
-    const cmdOutput = new Subject<string>();
+    const cmdOutput = new Subject<string>(); 
     // ipcRenderer.on('cmd-output', (event, data) => {
     //     cmdOutput.next(data);
     // });
-    return { isDockerRunning, cmdOutput };
+    return {
+        /**
+         * docker daemon是否在运行
+         */ 
+        isDockerRunning, 
+        /**
+         * 控制台输出
+         */ 
+        cmdOutput 
+    };
 });
